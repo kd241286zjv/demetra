149926755c92cf5bc7356dd59e317e028edbe41c
kd241286zjv <kd241286zjv@gmail.com>
2018-08-29 10:43:51 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+  var mainSwiper = new Swiper('.mainSlider-swiper-container', {
+    slidesPerView: 1,
+    loop:true,
+    autoplay: true,
+    navigation: {
+      nextEl: '.mainSlider-swiper-container .swiper-button-next',
+      prevEl: '.mainSlider-swiper-container .swiper-button-prev',
+    },
+  });
+  var catalogSwiper = new Swiper('.catalog-swiper-container', {
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  var partnersSwiper = new Swiper('.partners-swiper-container', {
+    slidesPerView: 5,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  ymaps.ready(init);
+  function init(){
+    var myMap = new ymaps.Map("map", {
+      center: [55.76, 37.64],
+      zoom: 7
+    });
+    var myPlacemark = new ymaps.Placemark([55.76, 37.64], {
+      hintContent: 'Содержимое всплывающей подсказки',
+      balloonContent: 'Содержимое балуна'
+    });
+    myMap.geoObjects.add(myPlacemark);
+  }
+
+  $('.selectSection ul li a').click(function(){
+    console.log('here');
+    $(this).toggleClass('activeTab');
+  })
+});
